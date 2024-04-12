669693cdd056e6a1f3b0f4035d1ceddd561064f2
NishantGangwar492 <nishantgangwar499@gmail.com>
2024-04-13 00:14:17 +0530

    kuch to kiya

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -4,52 +4,56 @@ import Google from '../../assets/photos/google.png'
 
 const Login = () => {
     return (
-        <div className='login container '>
-            <div className='login-headers'>
-                <div className='login-h1'>
-                    Welcome to
-                </div>
-                <div className='login-h2'>
-                    Events Misa
-                </div>
-            </div>
-            <br />
-            <div className='login-desc1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci venenatis luctus vitae viverra accumsan augue risus. Vulputate orci amet
-            </div>
-
-            <div className='login-input-box'>
-                <input type="email" placeholder='Email' />
-                <br />
-                <input type="password" placeholder='Password' />
-            </div>
-
-            <button className='login-btn'>
-                Login
-            </button>
-            <br />
-            
-            <section>
-                <div>
-                    Don't have an account?
-                    <a className='px-2'>
-                        Sign up
-                    </a>
-                </div>
-                <div style={{
-                    marginTop: 10,
-                    marginBottom: 15
-                }}>
-                    or
-                </div>
-                <div className='login-google-box'>
-                    <div className='login-google'>
-                        <img src={Google} className='google-icon'
-                        />
-                        Continue with google
+        <div className='container'>
+            <br/>
+            <br/>
+            <div className='login '>
+                <div className='login-headers'>
+                    <div className='login-h1'>
+                        Welcome to
+                    </div>
+                    <div className='login-h2'>
+                        Events Misa
                     </div>
                 </div>
-            </section>
+                <br />
+                <div className='login-desc1'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci venenatis luctus vitae viverra accumsan augue risus. Vulputate orci amet
+                </div>
+
+                <div className='login-input-box'>
+                    <input type="email" placeholder='Email' />
+                    <br />
+                    <input type="password" placeholder='Password' />
+                </div>
+
+                <button className='login-btn'>
+                    Login
+                </button>
+                <br />
+
+                <section>
+                    <div>
+                        Don't have an account?
+                        <a className='px-2'>
+                            Sign up
+                        </a>
+                    </div>
+                    <div style={{
+                        marginTop: 10,
+                        marginBottom: 15
+                    }}>
+                        or
+                    </div>
+                    <div className='login-google-box'>
+                        <div className='login-google'>
+                            <img src={Google} className='google-icon'
+                            />
+                            Continue with google
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
